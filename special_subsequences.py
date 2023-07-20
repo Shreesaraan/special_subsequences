f1c06744b4b039a5668f7dbb5912b5528fca680a
Shree Dev <Shreedev2k3@gmail.com>
2023-07-20 13:07:20 +0530

    Add files via upload

---
+++ special_subsequences.py
@@ -0,0 +1,16 @@
+def subsequence(sequence):
+
+    s=sequence.lower()
+    count=0
+    result=0
+    for i in s:
+        if i=='a':
+            count+=1
+        elif i=='g':
+            result+=count
+
+    return result
+
+sequence=input()
+print(subsequence(sequence))
+
